2fc3c6d868409fc79bd9ee6b72321cbf2a9e43e0
font199 <fontbx@gmail.com>
2018-12-10 09:39:46 +0100

    acavat sessio 8

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -12,12 +12,12 @@ const pagesRoutes: Routes = [
         path : '', 
         component: PagesComponent,
         children: [
-            {path : 'dashboard', component: DashboardComponent},
-            {path : 'progress', component: ProgressComponent},
-            {path : 'graficas1', component: Graficas1Component},
-            {path : 'account-settings', component: AccoutSettingsComponent},
-            {path : 'promesas', component: PromesasComponent},
-            {path : 'rxjs', component: RxjsComponent},
+            {path : 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'}},
+            {path : 'progress', component: ProgressComponent, data: {titulo: 'Progress'}},
+            {path : 'graficas1', component: Graficas1Component, data: {titulo: 'Gráficas'}},
+            {path : 'account-settings', component: AccoutSettingsComponent, data: {titulo: 'Ajustes del tema'}},
+            {path : 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'}},
+            {path : 'rxjs', component: RxjsComponent, data: {titulo: 'RxJS'}},
             {path : '', redirectTo: '/dashboard', pathMatch: 'full'}
         ]
     }
--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styles: []
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscripcion : Subscription;
 
   constructor() { 
  
-    this.regresaObservable().pipe(
-      retry(4) //li diem que si falla ho torni a provar 4 cops mes avans de ensenyar l'error
-    )
+    // .pipe(
+    //   retry(4) //li diem que si falla ho torni a provar 4 cops mes avans de ensenyar l'error
+    // )
+
+    this.subscripcion = this.regresaObservable()
     .subscribe(  //L'observador ens pot d'onar 3 respostes
       numero => console.log('Subs ',numero), //en el moment que fa el obs.next(contador)
       error => console.log('Error en el obs ',error), //si hi ha algun error
@@ -25,29 +29,49 @@ export class RxjsComponent implements OnInit {
   ngOnInit() {
   }
 
-  regresaObservable(): Observable<number>{ //podem indicar el que retorna l'observable
+  ngOnDestroy(){ // En el moment que fem un canvi de pagina es crida el destroy 
+    //i ens desubscribim perque ja no volem continuar observant
+    this.subscripcion.unsubscribe();
+  }
+
+  regresaObservable(): Observable<any>{ //podem indicar el que retorna l'observable
 
     return new Observable( observer => {
 
       let contador = 0;
 
       let intervalo = setInterval( () => {
-        contador ++;
-        observer.next(contador);
+        contador++;
 
-        if(contador === 3){
-          clearInterval(intervalo);
-          observer.complete();
-        }
+        let salida = {
+          valor: contador
+        };
 
-        if(contador === 2){
-          //clearInterval(intervalo); 
-          observer.error('Auxilio');
-        }
+        observer.next(salida); 
+
+        // if(contador === 3){
+        //   clearInterval(intervalo);
+        //   observer.complete();
+        // }
+
+        // if(contador === 2){
+        //   //clearInterval(intervalo); 
+        //   observer.error('Auxilio');
+        // }
 
       }, 1000);
 
-    });
+    }).pipe(
+      map( (resp: any) => resp.valor),
+      filter( (valor , index) => {
+        if((valor % 2) === 1){//impar
+          return true;
+        }else{
+          return false;
+        }
+      })
+      
+    );
 
   }
 
